4eb14047662c209909e5b094d98e0e0534cbbf03
Thibault Pouch <thibault.pouch@epitech.eu>
2025-04-23 10:32:31 +0200

    [feat] add Ray class with constructors and methods for origin and direction

---
+++ src/Ray.cpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2025
+** B_OOP_400_LIL_4_1_raytracer_thibault_pouch
+** File description:
+** Ray
+*/
+
+#include "Ray.hpp"
+using namespace RayTracer;
+using namespace Math;
+
+Ray::Ray(const Point3D& origin, const Vector3D& direction)
+{
+    this->_origin = origin;
+    this->_direction = direction;
+}
+
+Point3D Ray::GetOrigin()
+{
+    return this->_origin;
+}
+
+Vector3D Ray::GetDirection()
+{
+    return this->_direction;
+}
+
+Point3D Ray::at(double t) const
+{
+    return _origin + _direction * t;
+}
---
+++ src/Ray.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2025
+** B_OOP_400_LIL_4_1_raytracer_thibault_pouch
+** File description:
+** Ray
+*/
+
+#ifndef RAY_HPP_
+#define RAY_HPP_
+
+#include "Math/Point3D.hpp"
+#include "Math/Vector3D.hpp"
+
+namespace RayTracer {
+    class Ray {
+    public:
+        Ray(const Math::Point3D& origin, const Math::Vector3D& direction);
+        Ray() = default;
+
+        Math::Point3D GetOrigin();
+        Math::Vector3D GetDirection();
+
+        Math::Point3D at(double t) const;
+    private:
+        Math::Point3D _origin;
+        Math::Vector3D _direction;
+    };
+} // namespace RayTracer
+
+#endif // RAY_HPP_
