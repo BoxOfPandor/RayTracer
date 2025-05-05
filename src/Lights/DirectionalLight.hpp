2205ea5ca9a22b9c28968ae6d0785f0c2d862c3b
Thibault Pouch <thibault.pouch@epitech.eu>
2025-05-05 13:16:57 +0200

    [feat] add DirectionalLight class with direction, color, intensity, and shadow detection

---
+++ src/Lights/DirectionalLight.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2025
+** B_OOP_400_LIL_4_1_raytracer_thibault_pouch
+** File description:
+** DirectionalLight.hpp
+*/
+
+#ifndef DIRECTIONALLIGHT_HPP
+#define DIRECTIONALLIGHT_HPP
+
+#include "ILight.hpp"
+
+namespace RayTracer {
+    class DirectionalLight : public ILight {
+    public:
+        DirectionalLight(const Math::Vector3D& direction, const Math::Vector3D& color, double intensity = 1.0);
+
+        Math::Vector3D getDirection(const Math::Point3D& point) const override;
+        double getIntensity(const Math::Point3D& point) const override;
+        Math::Vector3D getColor() const override;
+        bool isShadowed(const Math::Point3D& point, const Scene& scene) const override;
+
+    private:
+        Math::Vector3D _direction;
+        Math::Vector3D _color;
+        double _intensity;
+    };
+}
+
+#endif // DIRECTIONALLIGHT_HPP
+
