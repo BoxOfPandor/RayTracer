d3c239991d3755b7eac99d19b88ea313cc30eebe
Thibault Pouch <thibault.pouch@epitech.eu>
2025-05-05 10:42:26 +0200

    [feat] implement Sphere class with intersection, normal calculation, and transformation methods

--- src/Primitives/Sphere.cpp
+++ src/Primitives/Sphere.cpp
@@ -6,33 +6,90 @@
 */
 
 #include "Sphere.hpp"
+#include <cmath>
+
 using namespace RayTracer;
+using namespace Math;
 
-Sphere::Sphere()
+Sphere::Sphere(const Math::Point3D& origin, double radius, const Material& material)
+    : _origin(origin), _radius(radius), _material(material)
 {
-    this->_origin = Math::Point3D(0, 0, 0);
-    this->_radius = 10;
 }
 
-Sphere::Sphere(const Math::Point3D& origin, double radius)
+bool Sphere::hits(const Ray& ray) const
 {
-    this->_origin = origin;
-    this->_radius = radius;
+    double t;
+    return getIntersection(ray, t);
 }
 
-bool Sphere::hit(const Ray& ray) const
+bool Sphere::getIntersection(const Ray& ray, double& t) const
 {
-    Math::Vector3D originToSphere(
+    Vector3D originToSphere(
         ray.getOrigin().getX() - _origin.getX(),
         ray.getOrigin().getY() - _origin.getY(),
         ray.getOrigin().getZ() - _origin.getZ()
     );
 
-    double a = Math::Vector3D::dot(ray.getDirection(), ray.getDirection());
-    double b = 2.0 * Math::Vector3D::dot(ray.getDirection(), originToSphere);
-    double c = Math::Vector3D::dot(originToSphere, originToSphere) - _radius * _radius;
+    double a = Vector3D::dot(ray.getDirection(), ray.getDirection());
+    double b = 2.0 * Vector3D::dot(ray.getDirection(), originToSphere);
+    double c = Vector3D::dot(originToSphere, originToSphere) - _radius * _radius;
 
     double discriminant = (b * b) - 4 * a * c;
 
-    return discriminant >= 0;
+    if (discriminant < 0) {
+        return false;
+    }
+
+    double sqrtDiscriminant = std::sqrt(discriminant);
+    double t1 = (-b - sqrtDiscriminant) / (2 * a);
+    double t2 = (-b + sqrtDiscriminant) / (2 * a);
+
+    if (t1 > 0.001) {
+        t = t1;
+        return true;
+    }
+    if (t2 > 0.001) {
+        t = t2;
+        return true;
+    }
+    
+    return false;
+}
+
+Vector3D Sphere::getNormalAt(const Point3D& point) const
+{
+    Vector3D normal(
+        point.getX() - _origin.getX(),
+        point.getY() - _origin.getY(),
+        point.getZ() - _origin.getZ()
+    );
+    
+    double length = normal.length();
+    if (length > 0) {
+        return normal / length;
+    }
+    
+    return Vector3D(1, 0, 0);
+}
+
+void Sphere::translate(const Vector3D& translation)
+{
+    _origin = _origin + translation;
+}
+
+void Sphere::rotateX(double angle)
+{
+}
+
+void Sphere::rotateY(double angle)
+{
+}
+
+void Sphere::rotateZ(double angle)
+{
+}
+
+const Material& Sphere::getMaterial() const
+{
+    return _material;
 }
